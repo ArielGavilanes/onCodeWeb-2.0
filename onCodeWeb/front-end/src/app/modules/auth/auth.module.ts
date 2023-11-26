7fc1b780ca3e1d7af9a09d8122cd30b346d6ce6f
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-11-25 22:10:27 -0500

    adding bootstrap

--- onCodeWeb/front-end/src/app/modules/auth/auth.module.ts
+++ onCodeWeb/front-end/src/app/modules/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BackgroundRowComponent } from './components/register/components/background-row/background-row.component';
+import { SelectRolComponent } from './components/register/components/select-rol/select-rol.component';
+import { EnterDataComponent } from './components/register/components/enter-data/enter-data.component';
+import { CreateUserComponent } from './components/register/components/create-user/create-user.component';
 
 
 
 @NgModule({
   declarations: [
+    LoginComponent,
     RegisterComponent,
-    LoginComponent
+    BackgroundRowComponent,
+    SelectRolComponent,
+    EnterDataComponent,
+    CreateUserComponent
   ],
   imports: [
     CommonModule
---
+++ onCodeWeb/front-end/src/app/modules/auth/components/register/components/background-row/background-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-row',
+  templateUrl: './background-row.component.html',
+  styleUrls: ['./background-row.component.css']
+})
+export class BackgroundRowComponent {
+
+}
