a13b15f38bf83957e89ed2bc743cce13e1c3849e
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-11-26 01:02:32 -0500

    consuming roles.service

--- onCodeWeb/front-end/src/app/modules/auth/components/register/components/select-rol/select-rol.component.ts
+++ onCodeWeb/front-end/src/app/modules/auth/components/register/components/select-rol/select-rol.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Roles } from 'src/app/database/interfaces/roles.interface';
+import { AuthService } from 'src/app/modules/auth/auth.service';
 
 @Component({
   selector: 'register-select-rol',
   templateUrl: './select-rol.component.html',
   styleUrls: ['./select-rol.component.css']
 })
-export class SelectRolComponent {
+export class SelectRolComponent implements OnInit {
 
+  roles: Roles[] = []
+  
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.getRoles()
+  }
+
+  getRoles() {
+    this.roles = this.authService.roles
+  }
 }
