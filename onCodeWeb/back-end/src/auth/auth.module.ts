4d26f536d9006d21022a24ecf1b67ec35651989e
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-12-01 01:27:50 -0500

    fixing

--- onCodeWeb/back-end/src/auth/auth.module.ts
+++ onCodeWeb/back-end/src/auth/auth.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthUsuarioController } from './controllers/auth-usuario/auth-usuario.controller';
+import { AuthUsuarioService } from './services/auth-usuario/auth-usuario.service';
+import { UsuariosModule } from 'src/on-code-web/usuarios/usuarios.module';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+    imports: [
+        UsuariosModule,
+        JwtModule.register({
+          secret: 'oCW00tknn', // Reemplaza con tu clave secreta
+          signOptions: { expiresIn: '2h' }, // Configura la expiración en 2 horas
+        })],
+    controllers: [AuthUsuarioController],
+    providers: [AuthUsuarioService]
+})
 export class AuthModule {}
--- onCodeWeb/back-end/src/on-code-web/usuarios/usuarios.module.ts
+++ onCodeWeb/back-end/src/on-code-web/usuarios/usuarios.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UsuarioService } from './services/usuario/usuario.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Usuarios } from './models/usuarios.model';
 
-@Module({})
-export class UsuariosModule {}
+@Module({
+    imports: [
+        SequelizeModule.forFeature([Usuarios])
+    ],
+    exports: [
+        UsuarioService
+    ],
+    providers: [
+        UsuarioService
+    ]
+})export class UsuariosModule {}
