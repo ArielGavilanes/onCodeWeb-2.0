439ffa0dbac88ad8fbff9ec73177bdf7731c50a0
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-12-07 15:58:39 -0500

    implementing cursos service

--- onCodeWeb/front-end/src/app/modules/cursos/components/components/content/content.component.ts
+++ onCodeWeb/front-end/src/app/modules/cursos/components/components/content/content.component.ts
@@ -9,7 +9,8 @@ import { CursosService } from '../../../cursos.service';
 })
 export class ContentComponent implements OnInit{
 
-  contenido: any;
+  banner: any;
+  contenidos: any;
 
   constructor (
     private cursosService: CursosService,
@@ -21,13 +22,20 @@ export class ContentComponent implements OnInit{
   ngOnInit(): void {
     const idCursoParam = this.route.snapshot.paramMap.get('id_curso');
     this.id_curso = idCursoParam ? +idCursoParam : 0;
+    console.log(this.id_curso)
 
+    console.log('Hola mundo')
+    this.getCursosContent()
   }
 
   getCursosContent() {
     this.cursosService.getCursoContent(this.id_curso).subscribe (
       (res: any) => {
         console.log(res)
+        this.banner = res.curso
+        this.contenidos = res.contenidos
+        console.log(this.banner)
+        console.log(this.contenidos)
       },
       (err: Error) => {
         console.log(err)
