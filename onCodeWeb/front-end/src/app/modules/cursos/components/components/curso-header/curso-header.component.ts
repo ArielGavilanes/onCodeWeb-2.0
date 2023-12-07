45532f08da8e294dc2e9cc6358495556a4190973
arielGs_Dev <arielogavilanes1534@gmail.com>
2023-12-07 08:29:52 -0500

    implementing cursos service

--- onCodeWeb/front-end/src/app/modules/cursos/components/components/curso-header/curso-header.component.ts
+++ onCodeWeb/front-end/src/app/modules/cursos/components/components/curso-header/curso-header.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CursosService } from '../../../cursos.service';
 
 @Component({
   selector: 'curso-curso-header',
   templateUrl: './curso-header.component.html',
   styleUrls: ['./curso-header.component.css']
 })
-export class CursoHeaderComponent {
+export class CursoHeaderComponent implements OnInit {
+  banner: any;
+  contenido: any;
 
+  constructor (
+    private cursosService: CursosService,
+    private route: ActivatedRoute
+  ) {
+  }
+  id_curso: number = 0;
+
+  ngOnInit(): void {
+    const idCursoParam = this.route.snapshot.paramMap.get('id_curso');
+    this.id_curso = idCursoParam ? +idCursoParam : 0;
+    console.log(this.id_curso)
+
+    console.log('Hola mundo')
+    this.getCursosContent()
+  }
+
+  getCursosContent() {
+    this.cursosService.getCursoContent(this.id_curso).subscribe (
+      (res: any) => {
+        console.log(res)
+        this.banner = res.curso
+        this.contenido = res.contenidos
+        // console.log(this.banner)
+        // console.log(this.contenido)
+      },
+      (err: Error) => {
+        console.log(err)
+      }
+    )
+  }
 }
